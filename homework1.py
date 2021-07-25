0795812e9ad67fadcd939e57ea235b6bd6c8bd48
Aghvan2345 <abgaryanaghvan15@gmail.com>
2021-07-25 17:39:25 +0300

    Add files

---
+++ homework1.py
@@ -0,0 +1,7 @@
+#home work one
+#this code prints  letters from the middle of the line 
+
+b = "hedroellektrokayan"
+c = int(len(b)/2-1)
+d = c+3
+print(b[c:d])
---
+++ homework2.py
@@ -0,0 +1,7 @@
+#this code adds a third word in two words
+a = "LeonelMessi"
+b = "Andres"
+c = int(len(a)/2+1)
+x = a[0:c]
+d = a[c:len(a)]
+print(x,b,d)
